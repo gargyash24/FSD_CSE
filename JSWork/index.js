17f83140f51be55804b3679623ba0553e8144268
Gargyash24 <har06g24@gmail.com>
2024-10-17 11:39:45 +0530

    uploaded

---
+++ JSWork/index.js
@@ -0,0 +1,38 @@
+// console.log("hiii")
+// var a=12;
+// console.log(a)
+// if(a>10){
+//     a=23
+//     console.log("a inside "+a)
+// }
+// console.log("a outside "+a)
+
+// let a = 12
+// let v = "12"
+// console.log(typeof a)
+// console.log(typeof v)
+// let today = Date()
+// console.log(today)
+// if(a===v){
+//     console.log("Welcome")
+// }
+// else{
+//     console.log("Hello")
+// }
+// let myname="yash"
+// let college="ABESEC"
+// let result = `hi my name is ${myname} and college is ${college}`
+// console.log(result)
+// const student={
+//     name:"Yash",
+//     college:"ABES",
+//     course:"B.Tech"
+// }
+// console.log(student)
+function greet(msg="welcome"){
+    console.log("hiiiiiii "+msg)
+}
+greet()
+var bhai = 5;
+console.log(`hai kaisa hai ${bhai}`);
+// const greet()=> console.log("saurav");
